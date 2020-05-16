7e6581fbb79bd46c8a617b3d6d20251cf2a40170
estevaojneto <estevaojadanhi@gmail.com>
2020-05-16 19:47:05 -0400

    (C) Added placeholder for Select Sort in C

---
+++ C/SortingAlgorithms/select.c
@@ -0,0 +1,42 @@
+/******************************************************************************
+This is Select Sort.
+*******************************************************************************/
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
+void generate_array(int *array, int array_size){
+    srand(time(0));
+    int i;
+    for(i = 0; i < array_size; i++)
+        array[i] = rand() % 100;
+    return;
+}
+
+void print_array(int *array, int array_size){
+    for(int i = 0; i < array_size; i++)
+        printf("%d ", array[i]);
+    printf("\n");
+}
+
+void select_sort(int *array, int array_size){
+    
+}
+
+int main(void) 
+{
+    int array_size;
+    printf("SHELL SORT\n");
+    printf("Enter array size:");
+    scanf("%d", &array_size);
+    int array[array_size];
+    generate_array(&array[0], array_size);
+    
+    printf("Original array:");
+    print_array(&array[0], array_size);
+    select_sort(&array[0], array_size);
+    printf("Sorted array:");
+    print_array(&array[0], array_size);
+    return 0; 
+} 
