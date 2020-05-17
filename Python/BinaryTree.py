c9bfc0b69c30a1be3eadb17a8157315a4f67f9b5
estevaojneto <estevaojadanhi@gmail.com>
2020-05-17 11:20:24 -0400

    (C) Added Insert Sort

---
+++ Python/BinaryTree.py
@@ -0,0 +1,30 @@
+class Node:
+	def __init__(self, value, left, right):
+		self.value = value
+		self.left = left
+		self.right = right
+	
+	def print_node(self):
+		print(self.value)
+
+class Tree:
+	root = Node(None, None, None)
+	def set_root(self, root):
+		self.root = root
+
+loop = True
+options = {
+	"0": "Você escolheu zero",
+	"1": "Você escolheu 1",
+	"2": "Você escolheu 3"
+}
+while(loop):
+	print("0 - Add Node")
+	print("1 - Delete Node")
+	print("2 - Print Tree")
+	print("Any - Exit")
+	sel = input("Insert option:")
+	try:
+	    result = options[sel]
+	except KeyError:
+	    loop = False
