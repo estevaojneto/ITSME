25121d4506f55009f2f6d050d0aa1f71bb635d86
estevaojneto <estevaojadanhi@gmail.com>
2020-05-21 03:24:29 -0400

    (JS) Small corrections in cards2.js (identation, returned value)

--- Javascript/cards2.js
+++ Javascript/cards2.js
@@ -9,17 +9,17 @@ function deal_card(){
 	if(deck.length < 1)
 		card_name = "(no more cards)"
 	else{
-    var min = Math.ceil(0);
-    var max = Math.floor(deck.length);
+		var min = Math.ceil(0);
+		var max = Math.floor(deck.length);
 		var card_index = Math.floor(Math.random() * (max - min)) + min;
 		var card_no = parseInt(deck[card_index]%13);
 		//card 2 is the third card from the first suit; card 40 is the second card from the last suit, etc
 		var card_suit_id = parseInt(deck[card_index]/13);
 		//cards 0...12: A,2,3...J,Q,K of Hearts; cards 13...25:  1...K of Spades; etc
 		deck.pop(card_index);
-		var card_name = card_no + ' of ' + suits[card_suit_id];
-		return card_name;
+		var card_name = card_no + ' of ' + suits[card_suit_id];		
 	}
+	return card_name;
 }
 
 for(i = 0; i < 52; i++)
